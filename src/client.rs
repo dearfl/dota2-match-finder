583c917f384ef2aaf689fac1045f9eb497906764
dearfl <i@flr.me>
2024-10-29 22:55:42 +0800

    <dev> add client impl and main logic

---
+++ src/client.rs
@@ -0,0 +1,70 @@
+use reqwest::{Proxy, StatusCode};
+use thiserror::Error;
+
+use crate::model::{MatchHistory, MatchHistoryResponse};
+
+#[derive(Error, Debug)]
+pub enum ClientError {
+    #[error("Failed to create proxy from provided proxy scheme: {0}.")]
+    ProxyError(String),
+    #[error("Failed to create reqwest client: {0}.")]
+    ConstructError(reqwest::Error),
+    #[error("Failed to retrive result from web API: {0}")]
+    ConnectionError(#[from] reqwest::Error),
+    #[error("Failed to decode web API response: {0}")]
+    DecodeError(serde_json::Error, String),
+    #[error("Too Many Requests")]
+    TooManyRequests,
+}
+
+pub struct Client {
+    client: reqwest::Client,
+    key: String,
+}
+
+impl Client {
+    const URL_GET_MATCH_HISTORY_BY_SEQUENCE_NUM: &str =
+        "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/v1";
+
+    pub fn new(key: String, proxy: Option<&str>) -> Result<Self, ClientError> {
+        let builder = reqwest::Client::builder();
+        let builder = match proxy {
+            Some(proxy) => {
+                let proxy =
+                    Proxy::all(proxy).map_err(|_| ClientError::ProxyError(proxy.to_string()))?;
+                builder.proxy(proxy)
+            }
+            None => builder,
+        };
+        let client = builder.build().map_err(ClientError::ConstructError)?;
+        Ok(Self { client, key })
+    }
+
+    pub async fn get_matches(
+        &self,
+        start_idx: u64,
+        count: u8,
+    ) -> Result<MatchHistory, ClientError> {
+        let req = self
+            .client
+            .get(Self::URL_GET_MATCH_HISTORY_BY_SEQUENCE_NUM)
+            .query(&[("key", &self.key)])
+            .query(&[("start_at_match_seq_num", start_idx)])
+            .query(&[("matches_requested", count)]);
+        let resp = req.send().await?;
+        match resp.status() {
+            StatusCode::TOO_MANY_REQUESTS => Err(ClientError::TooManyRequests),
+            StatusCode::OK => {
+                let content = resp.text().await?;
+                serde_json::from_str(&content)
+                    .map(|result: MatchHistoryResponse| result.result)
+                    .map_err(|err| ClientError::DecodeError(err, content))
+            }
+            _ => {
+                // we may want to handle some special cases
+                // so we intentionally leave this todo macro here
+                todo!()
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,69 @@
+mod client;
 mod model;
 
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use client::{Client, ClientError};
+
+#[derive(Parser)]
+pub struct Args {
+    #[arg(long)]
+    start_idx: u64,
+    #[arg(long)]
+    proxy: Option<String>,
+    keys: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let args = Args::parse();
+    let clients = args
+        .keys
+        .into_iter()
+        .map(|key| Client::new(key, args.proxy.as_deref()))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut index = args.start_idx;
+    let mut interval = std::time::Duration::from_millis(100);
+    const MIN_INTERVAL: std::time::Duration = std::time::Duration::from_millis(32);
+    const MAX_INTERVAL: std::time::Duration = std::time::Duration::from_millis(1000);
+
+    for clt in clients.iter().cycle() {
+        // traffic control?
+        tokio::time::sleep(interval).await;
+        log::info!("requesting");
+        match clt.get_matches(index, 100).await {
+            Ok(matches) => {
+                log::info!("success");
+                interval = std::cmp::max(interval * 4 / 5, MIN_INTERVAL);
+                index = matches
+                    .matches
+                    .iter()
+                    .fold(index, |init, md| std::cmp::max(init, md.match_seq_num) + 1);
+            }
+            Err(ClientError::DecodeError(err, content)) => {
+                // maybe valve have changed the json response format
+                // this is when things really goes wrong, we need to fix it manually
+                log::error!("decode error: {}", err);
+                std::fs::write("error.json", content)?;
+                return Err(err.into());
+            }
+            Err(ClientError::TooManyRequests) => {
+                // we're requesting API too frequently, so slowing it down a little.
+                log::warn!("too many requests");
+                interval = std::cmp::min(interval * 5, MAX_INTERVAL);
+                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+                continue;
+            }
+            Err(ClientError::ConnectionError(err)) => {
+                log::warn!("connection error: {}", err);
+                interval = std::cmp::min(interval * 2, MAX_INTERVAL);
+                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            }
+            Err(ClientError::ProxyError(_)) | Err(ClientError::ConstructError(_)) => unreachable!(),
+        }
+    }
+
+    Ok(())
 }
