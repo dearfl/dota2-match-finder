fa3fc1fc6878008b1d6758015c85319cda71af91
dearfl <i@flr.me>
2024-10-30 19:54:36 +0800

    <dev> add more cli args

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,16 @@ pub struct Args {
     clickhouse_database: Option<String>,
     #[arg(long)]
     clickhouse_table: Option<String>,
+    #[arg(long)]
+    clickhouse_user: Option<String>,
+    #[arg(long)]
+    clickhouse_password: Option<String>,
+    #[arg(long, default_value_t = 32)]
+    min_interval: u64,
+    #[arg(long, default_value_t = 1000)]
+    max_interval: u64,
+    #[arg(long, default_value_t = 10000)]
+    insert_batch_size: usize,
     keys: Vec<String>,
 }
 
@@ -32,8 +42,8 @@ async fn main() -> anyhow::Result<()> {
 
     let mut index = args.start_idx;
     let mut interval = std::time::Duration::from_millis(100);
-    const MIN_INTERVAL: std::time::Duration = std::time::Duration::from_millis(32);
-    const MAX_INTERVAL: std::time::Duration = std::time::Duration::from_millis(1000);
+    let min_interval: std::time::Duration = std::time::Duration::from_millis(args.min_interval);
+    let max_interval: std::time::Duration = std::time::Duration::from_millis(args.max_interval);
 
     let clickhouse_server = args
         .clickhouse_server
@@ -52,7 +62,17 @@ async fn main() -> anyhow::Result<()> {
         .with_url(clickhouse_server)
         .with_database(clickhouse_database);
 
-    let mut buffer = vec![];
+    let database = match args.clickhouse_user.as_ref() {
+        Some(user) => database.with_user(user),
+        _ => database,
+    };
+
+    let database = match args.clickhouse_password.as_ref() {
+        Some(password) => database.with_password(password),
+        _ => database,
+    };
+
+    let mut buffer = Vec::with_capacity(args.insert_batch_size + 100);
 
     for clt in clients.iter().cycle() {
         // traffic control?
@@ -61,7 +81,7 @@ async fn main() -> anyhow::Result<()> {
         match clt.get_matches(index, 100).await {
             Ok(matches) => {
                 log::debug!("success");
-                interval = std::cmp::max(interval * 4 / 5, MIN_INTERVAL);
+                interval = std::cmp::max(interval * 9 / 10, min_interval);
                 index = matches.matches.iter().fold(index, |init, mat| {
                     std::cmp::max(init, mat.match_seq_num + 1)
                 });
@@ -78,18 +98,18 @@ async fn main() -> anyhow::Result<()> {
             }
             Err(ClientError::ConnectionError(err)) => {
                 log::warn!("connection error: {}", err);
-                interval = std::cmp::min(interval * 2, MAX_INTERVAL);
+                interval = std::cmp::min(interval * 2, max_interval);
                 tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             }
             Err(ClientError::TooManyRequests) => {
                 // we're requesting API too frequently, so slowing it down a little.
                 log::warn!("too many requests");
-                interval = std::cmp::min(interval * 5, MAX_INTERVAL);
+                interval = std::cmp::min(interval * 5, max_interval);
                 tokio::time::sleep(std::time::Duration::from_secs(5)).await;
             }
             Err(ClientError::OtherResponse(status)) => {
                 log::error!("other response: {}", status);
-                interval = std::cmp::min(interval * 5, MAX_INTERVAL);
+                interval = std::cmp::min(interval * 5, max_interval);
                 tokio::time::sleep(std::time::Duration::from_secs(5)).await;
             }
             Err(ClientError::ProxyError(_)) | Err(ClientError::ConstructError(_)) => unreachable!(),
@@ -97,7 +117,7 @@ async fn main() -> anyhow::Result<()> {
 
         // manual batching
         log::debug!("buffer len: {}", buffer.len());
-        if buffer.len() >= 10000 {
+        if buffer.len() >= args.insert_batch_size {
             let mut insert = database.insert(clickhouse_table)?;
             for mat in &buffer {
                 insert.write(mat).await?;
