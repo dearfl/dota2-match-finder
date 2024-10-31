4ce2695563357d7d6ea8b58897a5db0495c43799
dearfl <i@flr.me>
2024-11-01 03:07:11 +0800

    <dev> abstract database operations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod client;
+mod database;
 mod model;
 mod rate;
 
 use std::{collections::HashMap, time::Duration};
 
-use chrono::{DateTime, NaiveDate, Utc};
 use clap::Parser;
+
 use client::{Client, ClientError};
+use database::Database;
+use model::{MatchId, Side};
 use rate::RateControl;
 
 #[derive(Parser)]
@@ -32,47 +35,6 @@ pub struct Args {
     keys: Vec<String>,
 }
 
-#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
-pub enum Side {
-    Radiant,
-    Dire,
-}
-
-impl From<u8> for Side {
-    fn from(value: u8) -> Self {
-        if value & 0x80u8 != 0 {
-            Self::Dire
-        } else {
-            Self::Radiant
-        }
-    }
-}
-
-fn new_database_connection(args: &Args) -> clickhouse::Client {
-    let clickhouse_server = args
-        .clickhouse_server
-        .as_ref()
-        .map_or("http://127.0.0.1:8123", String::as_str);
-    let clickhouse_database = args
-        .clickhouse_database
-        .as_ref()
-        .map_or("dota2", String::as_str);
-
-    let database = clickhouse::Client::default()
-        .with_url(clickhouse_server)
-        .with_database(clickhouse_database);
-
-    let database = match args.clickhouse_user.as_ref() {
-        Some(user) => database.with_user(user),
-        _ => database,
-    };
-
-    match args.clickhouse_password.as_ref() {
-        Some(password) => database.with_password(password),
-        _ => database,
-    }
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
@@ -84,49 +46,47 @@ async fn main() -> anyhow::Result<()> {
         .map(|key| Client::new(key, args.proxy.as_deref()))
         .collect::<Result<Vec<_>, _>>()?;
 
-    let mut index = args.start_idx;
+    let mut match_seq_num = args.start_idx;
 
     let mut rate = RateControl::new(args.min_interval, args.max_interval);
 
-    let _database = new_database_connection(&args);
-    let mut _buffer = HashMap::<NaiveDate, HashMap<Side, HashMap<u8, Vec<u64>>>>::new();
+    let database = Database::new(
+        args.clickhouse_server.as_deref(),
+        args.clickhouse_database.as_deref(),
+        args.clickhouse_user.as_deref(),
+        args.clickhouse_password.as_deref(),
+    );
 
-    for clt in clients.iter().cycle() {
+    let mut buffer = HashMap::<(Side, u8), Vec<MatchId>>::new();
+
+    for (idx, clt) in (0..100).cycle().zip(clients.iter().cycle()) {
+        log::debug!("rate control!");
         rate.wait().await;
-        match clt.get_match_history(index, 100).await {
+        // get_match_history is not reliable so we switch back to get_match_history_by_seq_num
+        match clt.get_match_history_full(match_seq_num, 100).await {
             Ok(matches) => {
-                if matches.matches.len() >= 100 {
-                    rate.speed_up();
-                } else {
-                    rate.slow_down();
-                }
-                index = matches.matches.iter().fold(index, |init, mat| {
-                    let date = DateTime::from_timestamp(mat.start_time as i64, 0)
-                        .map_or(NaiveDate::default(), |arg0: DateTime<Utc>| {
-                            DateTime::date_naive(&arg0)
-                        });
+                match_seq_num = matches.matches.iter().fold(match_seq_num, |init, mat| {
                     mat.players.iter().for_each(|player| {
                         let side: Side = player.player_slot.into();
-                        _buffer
-                            .entry(date)
+                        buffer
+                            .entry((side, player.hero_id))
                             .or_default()
-                            .entry(side)
-                            .or_default()
-                            .entry(player.hero_id)
-                            .or_default()
-                            .push(mat.match_id);
+                            .push(mat.match_id.into());
                     });
-                    std::cmp::max(init, mat.match_id + 1)
+                    std::cmp::max(init, mat.match_seq_num + 1)
                 });
+                rate.speed_up();
             }
             Err(ClientError::DecodeError(err, content)) => {
                 // maybe valve have changed the json response format
                 // this is when things really goes wrong, we need to fix it manually
                 log::error!("decode error: {}", err);
-                let filename = format!("{}-error.json", index);
-                std::fs::write(filename, content)?;
+                let filename = format!("{}-error.json", match_seq_num);
+                // intentionally ignore result of this write so we wont skip database write
+                let _ = std::fs::write(filename, content);
                 // we have to quit or else we'll end in a dead loop
-                return Err(err.into());
+                // return Err(err.into());
+                break;
             }
             Err(ClientError::ConnectionError(err)) => {
                 log::warn!("connection error: {}", err);
@@ -146,7 +106,17 @@ async fn main() -> anyhow::Result<()> {
             }
             Err(ClientError::ProxyError(_)) | Err(ClientError::ConstructError(_)) => unreachable!(),
         }
+
+        if idx == 99 {
+            log::info!("write database!");
+            database.save(&buffer).await?;
+            buffer.clear();
+        }
     }
+
+    log::info!("write database!");
+    database.save(&buffer).await?;
+    buffer.clear();
 
     Ok(())
 }
