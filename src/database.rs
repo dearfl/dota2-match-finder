6f42a9a0a9e12a331d085585e441040be1cf7ae5
dearfl <i@flr.me>
2024-11-01 03:07:11 +0800

    <dev> abstract database operations

---
+++ src/database.rs
@@ -0,0 +1,53 @@
+use std::collections::HashMap;
+
+use crate::model::{MatchId, Side};
+
+pub struct Database {
+    database: String,
+    client: clickhouse::Client,
+}
+
+impl Database {
+    pub fn new(
+        server: Option<&str>,
+        database: Option<&str>,
+        user: Option<&str>,
+        password: Option<&str>,
+    ) -> Self {
+        let server = server.unwrap_or("http://127.0.0.1:8123");
+        let database = database.unwrap_or("dota2").to_string();
+
+        let client = clickhouse::Client::default()
+            .with_url(server)
+            .with_database(&database);
+
+        let client = match user {
+            Some(user) => client.with_user(user),
+            _ => client,
+        };
+
+        let client = match password {
+            Some(password) => client.with_password(password),
+            _ => client,
+        };
+
+        Self { database, client }
+    }
+
+    pub async fn save(
+        &self,
+        matches: &HashMap<(Side, u8), Vec<MatchId>>,
+    ) -> Result<(), clickhouse::error::Error> {
+        for ((side, hero), matches) in matches {
+            let table = format!("match_{}_{}", side, hero);
+            let query = format!("CREATE TABLE IF NOT EXISTS {}.{} (match_id UInt64) ENGINE = MergeTree() ORDER BY match_id PARTITION BY intDiv(match_id, 100000000) PRIMARY KEY match_id;", &self.database, table);
+            self.client.query(&query).execute().await?;
+            let mut insert = self.client.insert(&table)?;
+            for mat in matches {
+                insert.write(mat).await?;
+            }
+            insert.end().await?;
+        }
+        Ok(())
+    }
+}
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,2 +1,44 @@
+use std::fmt::Display;
+
+use clickhouse::Row;
+use serde::{Deserialize, Serialize};
+
 pub mod full;
 pub mod partial;
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum Side {
+    Radiant,
+    Dire,
+}
+
+impl From<u8> for Side {
+    fn from(value: u8) -> Self {
+        if value & 0x80u8 != 0 {
+            Self::Dire
+        } else {
+            Self::Radiant
+        }
+    }
+}
+
+impl Display for Side {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            Side::Radiant => "radiant",
+            Side::Dire => "dire",
+        };
+        f.write_str(name)
+    }
+}
+
+#[derive(Row, Serialize, Deserialize)]
+pub struct MatchId {
+    pub match_id: u64,
+}
+
+impl From<u64> for MatchId {
+    fn from(value: u64) -> Self {
+        Self { match_id: value }
+    }
+}
