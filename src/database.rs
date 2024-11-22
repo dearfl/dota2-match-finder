9055b0ffe10b2b444efacf4b8852d90c24dd9b6b
dearfl <i@flr.me>
2024-11-22 19:08:00 +0800

    <dev> use bitmap from clickhouse

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use clickhouse::{error::Error, Client};
-use primitive_types::U256;
+use itertools::Itertools;
 
-use crate::dota2::{MatchDraft, MatchMask};
+use crate::dota2::MatchDraft;
 
 pub struct Database {
     database: String,
@@ -35,14 +35,14 @@ impl Database {
         let client = client.with_database(&database);
 
         let query = format!(
-            "CREATE TABLE IF NOT EXISTS {}.mask (
+            "CREATE TABLE IF NOT EXISTS {}.drafts (
                 match_id UInt64,
-                radiant UInt256,
-                dire UInt256,
+                radiant Tuple(UInt8, UInt8, UInt8, UInt8, UInt8),
+                dire Tuple(UInt8, UInt8, UInt8, UInt8, UInt8),
             )
             ENGINE = MergeTree()
             ORDER BY match_id
-            PARTITION BY intDiv(match_id, 100000000)
+            PARTITION BY intDiv(match_id, 10000000)
             PRIMARY KEY match_id;",
             &database
         );
@@ -58,50 +58,43 @@ impl Database {
         limit: usize,
         offset: usize,
     ) -> Result<Vec<MatchDraft>, Error> {
-        let to_mask = |heroes: &[u8]| {
-            let mut mask = U256::zero();
-            for &hero in heroes {
-                mask |= U256::one() << hero;
-            }
-            mask
+        let side_check = |side: &str, heroes: &[u8]| {
+            format!(
+                "(bitmapHasAll(bitmapBuild(array(untuple({}))), bitmapBuild([{}])))",
+                side,
+                heroes.iter().format(","),
+            )
         };
 
-        let mask1 = to_mask(team1);
-        let mask2 = to_mask(team2);
+        let (cond1, cond2) = match (team1.is_empty(), team2.is_empty()) {
+            (true, true) => return Ok(vec![]),
+            (true, false) => (side_check("radiant", team2), side_check("dire", team2)),
+            (false, true) => (side_check("radiant", team1), side_check("dire", team1)),
+            (false, false) => (
+                format!(
+                    "({} AND {})",
+                    side_check("radiant", team1),
+                    side_check("dire", team2)
+                ),
+                format!(
+                    "({} AND {})",
+                    side_check("radiant", team2),
+                    side_check("dire", team1)
+                ),
+            ),
+        };
 
-        let side_check =
-            |side: &str, mask: U256| format!("(bitOr({}, toUInt256('{}')) = {})", side, mask, side);
-
-        let cond1 = format!(
-            "({} AND {})",
-            side_check("radiant", mask1),
-            side_check("dire", mask2)
-        );
-        let cond2 = format!(
-            "({} AND {})",
-            side_check("radiant", mask2),
-            side_check("dire", mask1)
-        );
         let query = format!(
-            "SELECT ?fields FROM {}.mask WHERE ({} OR {}) ORDER BY match_id DESC LIMIT {} OFFSET {}",
+            "SELECT ?fields FROM {}.drafts WHERE ({} OR {}) ORDER BY match_id DESC LIMIT {} OFFSET {}",
             self.database, cond1, cond2, limit, offset
         );
-        self.query_match_draft(&query).await
+        self.client.query(&query).fetch_all().await
     }
 
-    pub async fn query_match_draft(&self, query: &str) -> Result<Vec<MatchDraft>, Error> {
-        let mut cursor = self.client.query(query).fetch::<MatchMask>()?;
-        let mut result = Vec::with_capacity(100);
-        while let Some(mask) = cursor.next().await? {
-            result.push((&mask).into());
-        }
-        Ok(result)
-    }
-
-    pub async fn save_match_masks(&self, masks: &[MatchMask]) -> Result<(), Error> {
-        let mut insert = self.client.insert("mask")?;
-        for mat in masks {
-            insert.write(mat).await?;
+    pub async fn save_match_masks(&self, drafts: &[MatchDraft]) -> Result<(), Error> {
+        let mut insert = self.client.insert("drafts")?;
+        for draft in drafts {
+            insert.write(draft).await?;
         }
         insert.end().await?;
         Ok(())
--- src/dota2/mod.rs
+++ src/dota2/mod.rs
@@ -1,32 +1,8 @@
-use std::fmt::Display;
-
 use clickhouse::Row;
-use primitive_types::U256;
 use serde::{Deserialize, Serialize};
 
 pub mod full;
 pub mod partial;
-
-mod u256 {
-    use primitive_types::U256;
-    use serde::{
-        de::{Deserialize, Deserializer},
-        ser::{Serialize, Serializer},
-    };
-
-    pub fn serialize<S: Serializer>(u: &U256, serializer: S) -> Result<S::Ok, S::Error> {
-        let buf = u.to_little_endian();
-        buf.serialize(serializer)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<U256, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let u: [u8; 32] = Deserialize::deserialize(deserializer)?;
-        Ok(U256::from_little_endian(&u))
-    }
-}
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub enum Side {
@@ -44,62 +20,35 @@ impl From<u8> for Side {
     }
 }
 
-impl Display for Side {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = match self {
-            Side::Radiant => "radiant",
-            Side::Dire => "dire",
-        };
-        f.write_str(name)
-    }
-}
-
-#[derive(Row, Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct MatchMask {
-    pub match_id: u64,
-    #[serde(with = "u256")]
-    pub radiant: U256,
-    #[serde(with = "u256")]
-    pub dire: U256,
-}
-
-impl From<&full::Match> for MatchMask {
-    fn from(value: &full::Match) -> Self {
-        let match_id = value.match_id;
-        let mut radiant = U256::zero();
-        let mut dire = U256::zero();
-        for player in &value.players {
-            let side: Side = player.player_slot.into();
-            let hero_mask = U256::one() << player.hero_id;
-            match side {
-                Side::Radiant => radiant |= hero_mask,
-                Side::Dire => dire |= hero_mask,
-            }
-        }
-        Self {
-            match_id,
-            radiant,
-            dire,
-        }
-    }
-}
-
 #[derive(Row, Debug, Clone, Serialize, Deserialize)]
 pub struct MatchDraft {
     pub match_id: u64,
-    pub radiant: Vec<u8>,
-    pub dire: Vec<u8>,
+    pub radiant: [u8; 5],
+    pub dire: [u8; 5],
 }
 
-impl From<&MatchMask> for MatchDraft {
-    fn from(value: &MatchMask) -> Self {
-        let to_array =
-            |val: U256| -> Vec<u8> { (0..=255u8).filter(|&idx| val.bit(idx.into())).collect() };
-
+impl From<&full::Match> for MatchDraft {
+    fn from(value: &full::Match) -> Self {
         let match_id = value.match_id;
-        let radiant = to_array(value.radiant);
-        let dire = to_array(value.dire);
-
+        let mut radiant = [0; 5];
+        let mut dire = [0; 5];
+        let mut ridx = 0;
+        let mut didx = 0;
+        value.players.iter().for_each(|player| {
+            let side: Side = player.player_slot.into();
+            // ideally there should be 5 randiant and 5 dire players and heroes
+            match side {
+                Side::Radiant if ridx < 5 => {
+                    radiant[ridx] = player.hero_id;
+                    ridx += 1;
+                }
+                Side::Dire if didx < 5 => {
+                    dire[didx] = player.hero_id;
+                    didx += 1;
+                }
+                _ => {}
+            }
+        });
         Self {
             match_id,
             radiant,
@@ -108,14 +57,9 @@ impl From<&MatchMask> for MatchDraft {
     }
 }
 
-#[derive(Row, Serialize, Deserialize)]
-pub struct MatchId {
-    pub match_id: u64,
-}
-
-impl From<u64> for MatchId {
-    fn from(value: u64) -> Self {
-        Self { match_id: value }
+impl From<full::Match> for MatchDraft {
+    fn from(value: full::Match) -> Self {
+        Self::from(&value)
     }
 }
 
@@ -124,20 +68,13 @@ mod tests {
     #[test]
     fn test() {
         use super::full::MatchHistoryResponse;
-        use super::{MatchDraft, MatchMask};
+        use super::MatchDraft;
 
         let parse = |file: &str| -> Vec<MatchDraft> {
             let content = std::fs::read_to_string(file).expect("Failed to read file");
             let resp = serde_json::from_str::<MatchHistoryResponse>(&content)
                 .expect("Failed to parse json response");
-            resp.result
-                .matches
-                .iter()
-                .map(|mat| {
-                    let mask: MatchMask = mat.into();
-                    (&mask).into()
-                })
-                .collect()
+            resp.result.matches.iter().map(Into::into).collect()
         };
         parse("./tests/1730303804-error.json");
         parse("./tests/6742154809-error.json");
